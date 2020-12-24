e91de944e5237c2e0340eb20790d78d2a515597d
Ayushman Gupta <ayushman1998.1998@gmail.com>
2020-12-24 14:28:57 +0530

    class components

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,54 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = (props) => {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+//Can't use hooks with classes
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        url: animal.url,
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    });
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>Fetching Data</h1>;
+    }
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      media,
+      name,
+      showModal,
+    } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
--- src/components/Pets.js
+++ src/components/Pets.js
@@ -1,23 +1,26 @@
 import React from "react";
+import { Link } from "@reach/router";
 
-const Pets = ({ animal, name, breed, location, media }) => {
+const Pets = ({ animal, name, breed, location, media, id }) => {
   let hero = "http://placecorgi.com/300/300";
   if (media.length) {
     hero = media[0].small;
   }
   return (
-    <div>
-      <h2>{animal}</h2>
-      <img src={hero} />
-      <p>
-        <strong>Name: </strong>
-        {name} <br />
-        <strong>Breed: </strong>
-        {breed} <br />
-        <strong>Location: </strong>
-        {location}
-      </p>
-    </div>
+    <Link to={`/details/${id}`}>
+      <div>
+        <h2>{animal}</h2>
+        <img src={hero} />
+        <p>
+          <strong>Name: </strong>
+          {name} <br />
+          <strong>Breed: </strong>
+          {breed} <br />
+          <strong>Location: </strong>
+          {location}
+        </p>
+      </div>
+    </Link>
   );
 };
 
